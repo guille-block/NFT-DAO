36815b222cfa17e9860fc3f32d9e7fc1d2698981
GuillermoFairbairn <fairbairnguillermo@gmail.com>
2022-10-07 22:24:36 -0300

    {TEST} test vote proposal succed

--- test/DaoTests.t.sol
+++ test/DaoTests.t.sol
@@ -5,6 +5,8 @@ import "forge-std/Test.sol";
 import "../src/DAOsNFTs.sol";
 import "../src/TimeLockNFTs.sol";
 import "../src/DAO.sol";
+import "../src/helpers/AddSub.sol";
+import "forge-std/console2.sol";
 
 abstract contract TimeLockTest is Test {
     TimeLockNFTs internal timeLockNFTs;
@@ -32,11 +34,56 @@ contract DaoTests is DaoToken {
         dao = new DAO("Test DAO", 4, nft, timeLockNFTs);
         vm.startPrank(address(timeLockNFTs));
         timeLockNFTs.grantRole(timeLockNFTs.TIMELOCK_ADMIN_ROLE(), address(dao));
+        vm.stopPrank();
     }
 
     function testChangedTheRoleToDao_TL() public {
+        vm.startPrank(address(timeLockNFTs));
         assertEq(timeLockNFTs.hasRole(timeLockNFTs.TIMELOCK_ADMIN_ROLE(),address(dao)), true);
+        vm.stopPrank();
     }
 
 
+    function testVoteProposal() public {
+        uint256 id = _createProposalForAddSub();
+        uint256 proposalState1 = uint256(dao.proposalSnapshot(id));
+        uint8 state1 = uint8(dao.state(id));
+        console2.log(state1);
+        vm.roll(block.number+2);
+        assertEq(uint256(dao.proposalSnapshot(id)), 2);
+        _voteOnProposal(id);
+        uint256 proposalState2 = uint256(dao.proposalSnapshot(id));
+        uint8 state2 = uint8(dao.state(id));
+        console2.log(state2);
+        vm.roll(dao.proposalDeadline(id)+1);
+        (uint256 votesAgainst, uint256 votesFor, uint256 votesAbstain) = dao.proposalVotes(id);
+        console2.log(votesFor);
+        assertEq(uint256(dao.state(id)), 4);
+    }
+
+    function _createProposalForAddSub() private returns (uint256) {
+        AddSub addSub = new AddSub();
+        vm.startPrank(address(1));
+        address[] memory targetAdd = new address[](1);
+        uint256[] memory targetVal = new uint256[](1);
+        bytes[] memory targetData = new bytes[](1);
+        string memory targetDesc = "Add 1 to number";
+
+        targetAdd[0] = address(addSub);
+        targetVal[0] = 0;
+        targetData[0] = abi.encodeWithSignature("add()");
+
+        nft.delegate(address(1));
+
+        uint256 proposalHashId = dao.propose(targetAdd, targetVal, targetData, targetDesc);
+        vm.stopPrank();
+        return proposalHashId;
+        
+    }
+
+    function _voteOnProposal(uint256 _proposalHashId) private {
+        vm.startPrank(address(1));
+        dao.castVote(_proposalHashId, 1);
+        vm.stopPrank();
+    }
 }
