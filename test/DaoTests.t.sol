720dca99eb6d4237a55e099e0ffa87fd40dffe10
GuillermoFairbairn <fairbairnguillermo@gmail.com>
2022-10-14 08:31:09 -0300

    {TEST} Add DAO Nft Factory test by queuing and executing a succesful new collection proposal

--- test/DaoTests.t.sol
+++ test/DaoTests.t.sol
@@ -7,6 +7,7 @@ import "../src/TimeLockNFTs.sol";
 import "../src/DAO.sol";
 import "../src/helpers/AddSub.sol";
 import "forge-std/console2.sol";
+import "../src/FactoryNFT.sol";
 
 abstract contract TimeLockTest is Test {
     TimeLockNFTs internal timeLockNFTs;
@@ -27,10 +28,10 @@ abstract contract DaoToken is TimeLockTest {
     
 }
 
-contract DaoTests is DaoToken {
+contract DaoSetup is DaoToken {
     DAO dao;
     AddSub addSub;
-    function setUp() public override {
+    function setUp() public virtual override {
         super.setUp();
         addSub = new AddSub();
         dao = new DAO("Test DAO", 4, nft, timeLockNFTs);
@@ -84,7 +85,7 @@ contract DaoTests is DaoToken {
         
     }
 
-    function _voteOnProposal(uint256 _proposalHashId) private {
+    function _voteOnProposal(uint256 _proposalHashId) internal {
         vm.startPrank(address(1));
         dao.castVote(_proposalHashId, 1);
         vm.stopPrank();
@@ -94,11 +95,9 @@ contract DaoTests is DaoToken {
     function _fakeVoteAndSuccedOnAddSub() private {
         uint256 id = _createProposalForAddSub();
         uint256 proposalState1 = uint256(dao.proposalSnapshot(id));
-        uint8 state1 = uint8(dao.state(id));
         vm.roll(block.number+2);
         _voteOnProposal(id);
         uint256 proposalState2 = uint256(dao.proposalSnapshot(id));
-        uint8 state2 = uint8(dao.state(id));
         vm.roll(dao.proposalDeadline(id)+1);
     }
 
@@ -116,5 +115,63 @@ contract DaoTests is DaoToken {
         vm.warp(block.timestamp+11);
         dao.execute(targetAdd, targetVal, targetData, keccak256(bytes(targetDesc)));
         assertEq(addSub.num(), 1);
+    }
+}
+
+
+
+contract DaoFactoryTests is DaoSetup {
+    FactoryNFT factoryNFT;
+    function setUp() public override {
+        super.setUp();
+        vm.prank(address(dao));
+        factoryNFT = new FactoryNFT();
+    }
+
+    function _proposeCollectionCreation(
+        string memory _name, 
+        string memory _contract_uri, 
+        string memory _token_uri) private returns(uint256) {
+            vm.startPrank(address(1));
+            address[] memory targetAdd = new address[](1);
+            uint256[] memory targetVal = new uint256[](1);
+            bytes[] memory targetData = new bytes[](1);
+            string memory targetDesc = "Create new ERC1155 collection";
+
+            targetAdd[0] = address(factoryNFT);
+            targetVal[0] = 0;
+            targetData[0] = abi.encodeWithSelector(FactoryNFT.createCollectionNFTERC1155.selector, "NFT COLLECTION 1", "https//:contract_url", "https//:token_url");
+            //bytes memory data = abi.encodeWithSelector(FactoryNFT.createCollectionNFTERC1155.selector, "NFT COLLECTION 1", "https//:contract_url", "https//:token_url");
+            console2.logBytes(targetData[0]);
+            nft.delegate(address(1));
+            uint256 proposalHashId = dao.propose(targetAdd, targetVal, targetData, targetDesc);
+            vm.stopPrank();
+            return proposalHashId;
+    }
+
+
+    function testSuccedERC1155CollectionCreation() public {
+        uint256 id = _proposeCollectionCreation("NFT COLLECTION 1", "https//:contract_url", "https//:token_url");
+        vm.roll(block.number+2);
+        _voteOnProposal(id);
+        vm.roll(dao.proposalDeadline(id)+1);
+
+        //recreate params
+        address[] memory targetAdd = new address[](1);
+        uint256[] memory targetVal = new uint256[](1);
+        bytes[] memory targetData = new bytes[](1);
+        string memory targetDesc = "Create new ERC1155 collection";
+
+        targetAdd[0] = address(factoryNFT);
+        targetVal[0] = 0;
+        targetData[0] = abi.encodeWithSelector(FactoryNFT.createCollectionNFTERC1155.selector, "NFT COLLECTION 1", "https//:contract_url", "https//:token_url");
+        
+        bytes memory data = abi.encodeWithSelector(FactoryNFT.createCollectionNFTERC1155.selector, "NFT COLLECTION 1", "https//:contract_url", "https//:token_url");
+            
+        dao.queue(targetAdd, targetVal, targetData, keccak256(bytes(targetDesc)));
+        vm.warp(block.timestamp+11);
+        address(factoryNFT).call{value:0}(data);
+        //dao.execute(targetAdd, targetVal, targetData, keccak256(bytes(targetDesc)));
+        assertEq(factoryNFT.countCollections(), 1);
     }
 }
